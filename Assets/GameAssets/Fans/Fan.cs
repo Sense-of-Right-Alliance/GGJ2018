dfa6ec674f9e6e8eb01163d65fafefe60e26a7cd
NarPar <2198644+NarPar@users.noreply.github.com>
2018-01-27 18:06:13 -0800

    NP - Dun whurry bout it

---
+++ Assets/GameAssets/Fans/Fan.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour {
+    [SerializeField]
+    float moveSpeed = 1.0f;
+
+    bool moving = false;
+    float moveT = 0.0f;
+    Vector3 startPos;
+    Vector3 endPos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void MoveTo(Vector3 pos)
+    {
+        moving = true;
+        moveT = 0.0f;
+
+        startPos = transform.position;
+        endPos = pos;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (moving) {
+            UpdateMoving();
+        }
+	}
+
+    void UpdateMoving() {
+        moveT += Time.deltaTime * moveSpeed;
+        if (moveT >= 1.0f)
+        {
+            moveT = 1.0f;
+        }
+
+        Vector3 newPos = Vector3.Lerp(startPos, endPos, moveT);
+
+        transform.position = newPos;
+    }
+}
