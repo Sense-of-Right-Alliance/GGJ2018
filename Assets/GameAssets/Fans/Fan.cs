bab28afd12a3d3c2c90cc122be9d24c557767c15
NarPar <2198644+NarPar@users.noreply.github.com>
2018-01-27 18:55:09 -0800

    NP - Basic fan spawning. They shoot out like a machine gun.

--- Assets/GameAssets/Fans/Fan.cs
+++ Assets/GameAssets/Fans/Fan.cs
@@ -6,40 +6,71 @@ public class Fan : MonoBehaviour {
     [SerializeField]
     float moveSpeed = 1.0f;
 
+    bool delaying = false;
+    float delayTimer = 0.0f;
+
     bool moving = false;
     float moveT = 0.0f;
     Vector3 startPos;
     Vector3 endPos;
+    float lerpSpeed = 0.0f;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 
-    public void MoveTo(Vector3 pos)
+    public void MoveTo(Vector3 pos, float delay)
+    {
+        startPos = transform.position;
+        endPos = pos;
+
+        float distance = Vector2.Distance(endPos, startPos);
+        lerpSpeed = moveSpeed / distance;
+
+        if (delay > 0)
+        {
+            delayTimer = delay;
+            delaying = true;
+        }
+        else
+        {
+            StartMove();
+        }
+    }
+
+    void StartMove()
     {
         moving = true;
         moveT = 0.0f;
-
-        startPos = transform.position;
-        endPos = pos;
     }
 
     // Update is called once per frame
     void Update () {
-		if (moving) {
+        if (delaying)
+        {
+            delayTimer -= Time.deltaTime;
+            if (delayTimer <= 0.0f)
+            {
+                delaying = false;
+                StartMove();
+            }
+        }
+		else if (moving) {
             UpdateMoving();
         }
 	}
 
     void UpdateMoving() {
-        moveT += Time.deltaTime * moveSpeed;
+        moveT += Time.deltaTime * lerpSpeed;
         if (moveT >= 1.0f)
         {
             moveT = 1.0f;
+            moving = false;
         }
 
         Vector3 newPos = Vector3.Lerp(startPos, endPos, moveT);
+        newPos.z = newPos.y; // sort that zzzzzzz
 
         transform.position = newPos;
     }
--- Assets/GameAssets/Fans/FanController.cs
+++ Assets/GameAssets/Fans/FanController.cs
@@ -3,14 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FanController : MonoBehaviour {
+    [SerializeField]
+    GameObject fanPrefab;
+    [SerializeField]
+    Transform spawnTransform;
+    [SerializeField]
+    Transform crowdCenterTransform;
+
+    Vector3 spawnPosition;
+    Vector3 crowdCenterPosition;
+
+    List<GameObject> fans = new List<GameObject>();
+
+    Dictionary<int, GameObject> playerFans = new Dictionary<int, GameObject>();
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        spawnPosition = (spawnTransform == null) ? Vector3.zero : spawnTransform.position;
+        crowdCenterPosition = (crowdCenterTransform == null) ? Vector3.zero : crowdCenterTransform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        // THIS IS ONLY TO TEST. DON'T WORRY
+		if (Input.GetKeyUp(KeyCode.Space))
+        {
+            SpawnNewCrowd(10);
+        }
 	}
+
+    void SpawnNewCrowd(int crowdSize)
+    {
+        for (int i = 0; i < crowdSize; i++)
+        {
+            //if (i > fans.Count)
+            //{
+                GameObject fan = CreateNewFan();
+
+                fan.transform.position = spawnPosition;
+
+                fan.GetComponent<Fan>().MoveTo(GetCrowdPosition(), i*0.06f);
+            //}
+        }
+    }
+
+    GameObject CreateNewFan()
+    {
+        GameObject newFan = (GameObject)Instantiate(fanPrefab, Vector3.zero, Quaternion.identity);
+
+        fans.Add(newFan);
+
+        return newFan;
+    }
+
+    Vector3 GetCrowdPosition()
+    {
+        Vector3 pos = crowdCenterPosition;
+
+        float range = 0.15f;
+
+        // To Do: Distribute position so the crowd is spread out and fans are not overlapping one another
+        pos.x += Random.Range(-range / 2.0f, range / 2.0f);
+        pos.y += Random.Range(-range / 2.0f, range / 2.0f);
+
+        return pos;
+    }
 }
